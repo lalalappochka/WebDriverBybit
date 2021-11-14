43668fe23c9be5921941e9d812dab5aeb041d22b
sanya lappo <sanyalappo16@gmail.com>
2021-11-15 00:09:16 +0300

    -test

--- ChromeDriverBybit/TestProject1/UnitTest1.cs
+++ ChromeDriverBybit/TestProject1/UnitTest1.cs
@@ -40,7 +40,7 @@ namespace ChromeDriverUnitTest
             //    driver.Manage().Cookies.AddCookie(new OpenQA.Selenium.Cookie(cookie.Name, cookie.Value));
             //}
             //driver.Navigate().Refresh();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(90);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(120);
             IWebElement LoginButton = driver.FindElement(By.XPath("//*[@id='uniFrameHeader']/div[2]/div[1]/span[1]"));
             LoginButton.Click();
             //IWebElement EmailField = driver.FindElement(By.XPath("//*[@id='__layout']/div/main/div/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div/div[1]/div[2]/input"));
@@ -62,22 +62,21 @@ namespace ChromeDriverUnitTest
             driver.Navigate().GoToUrl("https://testnet.bybit.com/user/assets/home");
             IWebElement Transfer = driver.FindElement(By.XPath("//*[@id='root']/div/main/aside/div/div[1]/div/button[3]"));
             Transfer.Click();
-            //IWebElement AccountSend = driver.FindElement(By.XPath("//*[@id='modal - root']/div[1]/div/div[2]/div[1]/div[1]"));
-            //AccountSend.Click();
-            //IWebElement ChoiceSendAccount = driver.FindElement(By.XPath("//*[@id='modal - root']/div[1]/div/div[2]/div[1]/div[1]/div[3]/div[4]"));
-            //ChoiceSendAccount.Click();
-            //IWebElement AccountReceive = driver.FindElement(By.XPath("//*[@id='modal-root']/div[1]/div/div[2]/div[1]/div[3]"));
-            //AccountSend.Click();
-            //IWebElement ChoiceReceiver = driver.FindElement(By.XPath("//*[@id='modal - root']/div[1]/div/div[2]/div[1]/div[3]/div[3]/div[2]"));
-            //ChoiceReceiver.Click();
-            //IWebElement Coin = driver.FindElement(By.XPath("//*[@id='modal - root']/div[1]/div/div[2]/div[3]/div"));
-            //Coin.Click();
-            //IWebElement ChoiceBTC = driver.FindElement(By.XPath("//*[@id='modal - root']/div[2]/div/div/div/div[2]/div[1]/div/div/div[4]"));
-            //ChoiceBTC.Click();
-            IWebElement TransferableAmount = driver.FindElement(By.XPath("//*[@id='modal-root']/div[1]/div/div[2]/div[5]"));
-            TransferableAmount.Click();
-
-            IWebElement ConfirmButton = driver.FindElement(By.XPath("//*[@id='modal - root']/div[1]/div/div[2]/div[6]/button"));
+            IWebElement SenderAccount = driver.FindElement(By.XPath("//*[@id='modal-root']/div[1]/div/div[2]/div[1]/div[1]"));
+            SenderAccount.Click();
+            IWebElement ChoiceSendAccount = driver.FindElement(By.XPath("//*[@id='modal-root']/div[1]/div/div[2]/div[1]/div[1]/div[3]/div[4]"));
+            ChoiceSendAccount.Click();
+            IWebElement AccountReceive = driver.FindElement(By.XPath("//*[@id='modal-root']/div[1]/div/div[2]/div[1]/div[3]"));
+            AccountReceive.Click();
+            IWebElement ChoiceReceiver = driver.FindElement(By.XPath("//*[@id='modal-root']/div[1]/div/div[2]/div[1]/div[3]/div[3]/div[2]"));
+            ChoiceReceiver.Click();
+            IWebElement Coin = driver.FindElement(By.XPath("//*[@id='modal-root']/div[1]/div/div[2]/div[3]/div"));
+            Coin.Click();
+            IWebElement ChoiceBTC = driver.FindElement(By.XPath("//*[@id='modal-root']/div[2]/div/div/div/div[2]/div[1]/div/div/div[4]"));
+            ChoiceBTC.Click();
+            IWebElement TransferableAmount = driver.FindElement(By.XPath("//*[@id='modal-root']/div/div/div[2]/div[5]/div/div/span[1]/input"));
+            TransferableAmount.SendKeys("0.2");
+            IWebElement ConfirmButton = driver.FindElement(By.XPath("//*[@id='modal-root']/div[1]/div/div[2]/div[6]/button"));
             ConfirmButton.Click();
 
 
